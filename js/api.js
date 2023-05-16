4d8eb50888eefe081ef4f3f0c30d459a7115adb0
devggui <devguiet@gmail.com>
2023-05-16 14:30:59 -0300

    feat: add loader

--- js/api.js
+++ js/api.js
@@ -1,13 +1,36 @@
+const content = document.getElementById('content')
+const loaders = document.getElementById('loaders')
+
 async function getAdvice() {
-  try {
+  showLoader()
+
+  try {    
     const id = document.getElementById('id')
     const advice = document.getElementById('advice')
 
-    const response = await axios.get('https://api.adviceslip.com/advice')
-
+    const response = await axios.get('https://api.adviceslip.com/advice')  
+  
     id.innerText = response.data.slip.id  
     advice.innerText = response.data.slip.advice  
+    
+    hideLoader()
   } catch (error) {
     console.log(error)
   }
+}
+
+function showLoader() {
+  loaders.classList.remove('hidden')
+  loaders.classList.add('flex')
+
+  content.classList.remove('flex')
+  content.classList.add('hidden')
+}
+
+function hideLoader() {
+  loaders.classList.remove('flex')
+  loaders.classList.add('hidden')
+
+  content.classList.remove('hidden')
+  content.classList.add('flex')
 }
